745ea64894abb36e545076812eaf78bb50dd8fd7
Amogh Kashyap <amoghk.dev@gmail.com>
2022-01-24 02:00:44 -0500

    data collection
    improved user input and added waste-data + data filtering based on sample size

--- data/datacapture.py
+++ data/datacapture.py
@@ -6,6 +6,7 @@ import csv
 
 
 csvlist = []
+mindatapoints = 2000
 
 port = "/dev/cu.usbserial-0001" #serial port of Arduino
 baud = 115200 #board runs at 115200 baud
@@ -21,12 +22,12 @@ print("Created file")
 #Ask user for input regarding impact
 userinput = input("is this an Impact?: ")
 
-if userinput == "t":
+if userinput == "t" or userinput == "y" or userinput == "yes":
     #impact = True
     csvlist.append("Yes")
 else:
     #impact = False
-    csvlist.append("Yes")
+    csvlist.append("No")
 
 # Wait for Start
 input("Press Enter to start")
@@ -38,33 +39,58 @@ print("2")
 time.sleep(1)
 print("1")
 
-for i in range(20):
-        print(str(ser.readline()))
 
+points = 0
 t_end = time.time() + 2
 starttime = time.time()
 
+
 while time.time() < t_end:
     #display the data to the terminal
-    
-    
     getData=str(ser.readline())
     data=getData[2:6]
     rawcount = time.time() - starttime
 
     ts = round(rawcount, 5)
 
-
     print(str(ts) + ", " + data)
-    locallist = [ts, data]
-    csvlist.append(locallist)
+    csvlist.append(data)
+    points = points +1
 
-print(csvlist)
 
-with open('data.csv', 'a', newline='') as csvobject:
-    writer_object = csv.writer(csvobject)
-    writer_object.writerow(csvlist)
-    csvobject.close()
+
+if points > mindatapoints:
+    difference =  points - mindatapoints
+
+    for i in range(difference):
+        csvlist.pop(1)
+
+
+    with open('data.csv', 'a', newline='') as csvobject:
+        writer_object = csv.writer(csvobject)
+        writer_object.writerow(csvlist)
+        csvobject.close()
+
+elif points < mindatapoints:
+    print("Less than 3500 points collected. Trashing data...")
+    
+
+    with open('wastedata.csv', 'a', newline='') as csvobject:
+        writer_object = csv.writer(csvobject)
+        writer_object.writerow(csvlist)
+        csvobject.close()
+            
+    print("Done.")
+
+
+
+
+
+
+
+
+
+
     
 
 '''
