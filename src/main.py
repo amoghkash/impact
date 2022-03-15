4cd362296b2b99b3d191205b39f8a01123e07b43
Amogh Kashyap <amoghk.dev@gmail.com>
2022-03-15 11:35:25 -0400

    sdjfadjkshfsjkhasdf
    dfsbjjkfwhfkjewhje

--- src/main.py
+++ src/main.py
@@ -4,11 +4,14 @@ import logger.logger as logger
 import time
 #import plot as visualizer
 import variable.const as constant
+import AppKit
+
 
 starttime = time.time()
 
-
-while(True):
+print('\a')
+x=1
+while(x==1):
     rawdata = input.getdata()
     data = processing.normalize(rawdata)
     magnitudevalue = float(processing.getmagnitude(data))
@@ -17,6 +20,10 @@ while(True):
     data.append(timestamp)
     print(data)
     logger.log(data, constant.datafile)
+    if(magnitudevalue>60):
+        x=0
+        print("Impact Detected")
+        AppKit.NSBeep()
     #visualizer.animate()
     
 
