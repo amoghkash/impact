5dd608905f64da2f9c5724c2cd98f9e4ec492120
amoghkash <94269573+amoghkash@users.noreply.github.com>
2022-06-11 13:47:08 -0400

    Final Commit Before Close
    Stable Version

--- src/main.py
+++ src/main.py
@@ -21,29 +21,7 @@ root.title("Player Monitoring Service")
 player1 = tk.Label(root, text="placeholder", borderwidth=2, relief="solid", font=('Helvetica', 50))
 player1.grid(column=0, row=1, padx=50, pady=10)
 
-player2 = tk.Label(root, text="placeholder", borderwidth=2, relief="solid", font=('Helvetica', 50))
-player2.grid(column=0, row=2, padx=50, pady=10)
 
-player3 = tk.Label(root, text="placeholder", borderwidth=2, relief="solid", font=('Helvetica', 50))
-player3.grid(column=0, row=3, padx=50, pady=10)
-
-player4 = tk.Label(root, text="placeholder", borderwidth=2, relief="solid", font=('Helvetica', 50))
-player4.grid(column=1, row=1, padx=50, pady=10)
-
-player5 = tk.Label(root, text="placeholder", borderwidth=2, relief="solid", font=('Helvetica', 50))
-player5.grid(column=1, row=2, padx=50, pady=10)
-
-player6 = tk.Label(root, text="placeholder", borderwidth=2, relief="solid", font=('Helvetica', 50))
-player6.grid(column=1, row=3, padx=50, pady=10)
-
-player7 = tk.Label(root, text="placeholder", borderwidth=2, relief="solid", font=('Helvetica', 50))
-player7.grid(column=3, row=1, padx=50, pady=10)
-
-player8 = tk.Label(root, text="placeholder", borderwidth=2, relief="solid", font=('Helvetica', 50))
-player8.grid(column=3, row=2, padx=50, pady=10)
-
-player9 = tk.Label(root, text="placeholder", borderwidth=2, relief="solid", font=('Helvetica', 50))
-player9.grid(column=3, row=3, padx=50, pady=10)
 
 print('\a')
 
@@ -67,23 +45,8 @@ def set_label():
 
 
     data1 = magnitudevalue
-    data2 = random.uniform(5.85,50.93)
-    data3 = random.uniform(5.85,50.93)
-    data4 = random.uniform(5.85,50.93)
-    data5 = random.uniform(5.85,50.93)
-    data6 = random.uniform(5.85,50.93)
-    data7 = random.uniform(5.85,50.93)
-    data8 = random.uniform(5.85,50.93)
-    data9 = random.uniform(5.85,50.93)
+    #data1 = random.uniform(5.85,50.93)
     player1['text'] = "Player 1: " + str(round(data1, 2))
-    player2['text'] = "Player 2: " + str(round(data2, 2))
-    player3['text'] = "Player 3: " + str(round(data3, 2))
-    player4['text'] = "Player 4: " + str(round(data4, 2))
-    player5['text'] = "Player 5: " + str(round(data5, 2))
-    player6['text'] = "Player 6: " + str(round(data6, 2))
-    player7['text'] = "Player 7: " + str(round(data7, 2))
-    player8['text'] = "Player 8: " + str(round(data8, 2))
-    player9['text'] = "Player 9: " + str(round(data9, 2))
     root.after(1, set_label)
     
 
--- src/variable/const.py
+++ src/variable/const.py
@@ -2,12 +2,12 @@ import os
 
 
 
-basedir = os.path.expanduser('~') + "/impact"
+basedir = os.path.expanduser('~') + "/projects/impact"
 
-datafile = os.path.expanduser('~') + "/impact/data/collection/data.csv"
+datafile = os.path.expanduser('~') + "/projects/impact/data/collection/data.csv"
 
-trainingfile = os.path.expanduser('~') + "/impact/data/training/train.csv"
+trainingfile = os.path.expanduser('~') + "/projects/impact/data/training/train.csv"
 
-testingfile = os.path.expanduser('~') + "/impact/data/testing/test.csv"
+testingfile = os.path.expanduser('~') + "/projects/impact/data/testing/test.csv"
 
-trashfile = os.path.expanduser('~') + "/impact/data/trash/data.csv"
+trashfile = os.path.expanduser('~') + "/projects/impact/data/trash/data.csv"
